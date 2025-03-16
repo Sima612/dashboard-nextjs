d106dd849b4f90a05772b11a2e86b90fa0318e2e
Sima Tran <64188273+Sima612@users.noreply.github.com>
2025-03-16 09:42:50 -0700

    fixed login

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,14 +1,24 @@
 "use client";
 
 import { libre } from "@/app/ui/fonts";
-import { AtSymbolIcon, KeyIcon } from "@heroicons/react/24/outline";
+import {
+  AtSymbolIcon,
+  ExclamationCircleIcon,
+  KeyIcon,
+} from "@heroicons/react/24/outline";
 import { ArrowRightIcon } from "@heroicons/react/20/solid";
 import { Button } from "./button";
 import { useActionState } from "react";
 import { authenticate } from "../lib/actions";
+import { useSearchParams } from "next/navigation";
 
 export default function LoginForm() {
-  const [formAction] = useActionState(authenticate, undefined);
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl") || "/dashboard";
+  const [errorMessage, formAction, isPending] = useActionState(
+    authenticate,
+    undefined
+  );
 
   return (
     <form action={formAction} className={`${libre.className} rounded-2xl`}>
@@ -55,9 +65,25 @@ export default function LoginForm() {
             </div>
           </div>
         </div>
-        <Button className="mt-4 w-full cursor-pointer">
+        <input type="hidden" name="redirectTo" value={callbackUrl} />
+        <Button
+          className="mt-4 w-full cursor-pointer"
+          aria-disabled={isPending}
+        >
           Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
         </Button>
+        <div
+          className="flex h-8 items-end space-x-1"
+          aria-live="polite"
+          aria-atomic="true"
+        >
+          {errorMessage && (
+            <>
+              <ExclamationCircleIcon className="h-5 w-5 text-red-500" />
+              <p className="text-sm text-red-500">{errorMessage}</p>
+            </>
+          )}
+        </div>
       </div>
     </form>
   );
