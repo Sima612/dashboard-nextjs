8d6c0355b3e07bde8c60097c36905baf3ea2dc56
Sima Tran <64188273+Sima612@users.noreply.github.com>
2025-03-16 15:53:49 -0700

    fixed delete

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -131,16 +131,8 @@ export async function updateInvoice(
 }
 
 export async function deleteInvoice(id: string) {
-  try {
-    await sql`
+  await sql`
     DELETE FROM invoices WHERE id = ${id}
     `;
-    revalidatePath("/dashboard/invoices");
-    return { message: "Deleted Invoice" };
-  } catch (error) {
-    console.error(error);
-    return {
-      message: "Database Error: Failed to Delete Invoice",
-    };
-  }
+  revalidatePath("/dashboard/invoices");
 }
